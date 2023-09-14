c6ae43b69499513146db2bfd53d6fcbf1020a49b
Victor <vitory.dong@gmail.com>
2023-09-14 18:42:09 +0800

    内存对齐测试

---
+++ memalign/mem_align_test.go
@@ -0,0 +1,11 @@
+package memalign
+
+import "testing"
+
+func TestBasicTypeAlign(t *testing.T) {
+	//BasicTypeAlign()
+	//StructTypeAlign()
+	//StructTypeAlign2()
+	//StructTypeAlign3()
+	StructTypeAlign4()
+}
