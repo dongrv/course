e495a6573db250ef8700085be3c65e915988f7a7
Victor <vitory.dong@gmail.com>
2023-09-21 20:03:18 +0800

    删除敏感信息

--- checksvn/main.go
+++ checksvn/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -40,8 +41,8 @@ var svnCMDs = map[CMD]func(*string) []Row{
 }
 
 func main() {
-	println(1)
-	svnURL := `svn://xxx.com`
+
+	svnURL := ``
 	username := ""
 	password := ""
 
@@ -51,21 +52,19 @@ func main() {
 		panic(err)
 	}
 	str = strings.TrimSpace(strings.ReplaceAll(str, splitLine, ""))
-	println(2, str)
 	results := svnCMDs[SvnShowLog](&str)
 	if len(results) == 0 {
 		return
 	}
 	currentLog := results[0] // 取最近行
 	if time.Now().Unix()-currentLog.Timestamp <= 5*60 {
-		msg := fmt.Sprintf("### 业务服Pre-release Branch有更新！\n >**提交：** %s\n >**注释：** %s\n >**时间：** %s\n\n 请及时处理 <@101>",
+		msg := fmt.Sprintf("### 业务服Pre-release Branch有更新！\n >**提交：** %s\n >**注释：** %s\n >**时间：** %s\n\n 请及时处理 <@118>",
 			currentLog.Author, currentLog.Comment, currentLog.DateTime)
-		println(3, msg)
 		notifyGroup(msg)
 	}
 }
 
-var notifyURL = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8fa40bf3-2f0d-4823-9085-fd4a91813740`
+var notifyURL = `` // 机器人通知
 
 type Markdown struct {
 	Content             string   `json:"content"`
@@ -111,20 +110,51 @@ type Row struct {
 
 func parseSvnLog(str *string) []Row {
 	msg := *str
-	logs := strings.Split(msg, "line")
-	var rows []Row
-	for i := 0; i < len(logs)/2; {
-		logs[i] = strings.TrimSpace(logs[i])
-		meta := TrimSlice(strings.Split(logs[i], "|"))
-		date, _, _ := strings.Cut(meta[2], "+0800")
-		timestamp, _ := time.ParseInLocation(time.DateTime, strings.TrimSpace(date), time.FixedZone("CST", 28800))
-		seconds := timestamp.Unix()
+	revisions := strings.Split(msg, "line")
+	var (
+		rows        []Row
+		offsetHours int64
+	)
+	for i := 0; i < len(revisions)/2; {
+		revisions[i] = strings.TrimSpace(revisions[i])
+		meta := TrimSlice(strings.Split(revisions[i], "|"))
+		times := strings.Split(meta[2], " ")
+		timeZone := times[2] // +0800
+		v := timeZone[1:]
+		iv, _ := strconv.Atoi(v)
+		iv /= 100
+		if timeZone[0] == '-' {
+			offsetHours = (int64(iv) + 8) * 3600 // +8对标东八区时间
+		} else {
+			var diff int64
+			if int64(iv)-8 >= 0 {
+				diff = -(int64(iv) - 8) // 东八区之前的时间减
+			} else {
+				diff = 8 - int64(iv) // 东八区之后的时间加
+			}
+			offsetHours = diff * 3600 // 和东八区时间相差时间
+		}
+
+		var cutSep string // 剪裁标识
+		if iv <= 9 {
+			cutSep = fmt.Sprintf("%c0%d", timeZone[0], iv*100)
+		} else {
+			cutSep = fmt.Sprintf("%c%d", timeZone[0], iv*100)
+		}
+
+		date, _, _ := strings.Cut(meta[2], cutSep)
+		timestamp, err := time.ParseInLocation(time.DateTime, strings.TrimSpace(date), time.FixedZone("CST", 28800))
+		if err != nil {
+			fmt.Printf("err %v\n", err)
+		}
+		seconds := timestamp.Unix() + offsetHours
+		date = time.Unix(seconds, 0).In(time.FixedZone("CST", 28800)).Format(time.DateTime) // 校准后的日期
 		row := Row{
 			Revision:  meta[0],
 			Author:    meta[1],
 			DateTime:  date,
 			Timestamp: seconds,
-			Comment:   strings.TrimSpace(logs[i+1]),
+			Comment:   strings.TrimSpace(revisions[i+1]),
 		}
 		rows = append(rows, row)
 		i += 2
@@ -167,9 +197,12 @@ func Command(args ...string) (string, error) {
 	if err = cmd.Wait(); err != nil {
 		return "", err
 	}
-	output, err := simplifiedchinese.GB18030.NewDecoder().String(string(bytes))
-	if err != nil {
-		return "", err
+	if runtime.GOOS == "windows" {
+		output, err := simplifiedchinese.GB18030.NewDecoder().String(string(bytes))
+		if err != nil {
+			return "", err
+		}
+		return output, nil
 	}
-	return output, nil
+	return string(bytes), nil
 }
