4a27fe31706dbd5fb6ddeefae1e16d906d2daae2
Victor <vitory.dong@gmail.com>
2024-01-29 16:59:58 +0800

    测试定时器泄露;

--- testtime/test_time.go
+++ testtime/test_time.go
@@ -2,6 +2,9 @@ package testtime
 
 import (
 	"fmt"
+	"net"
+	"net/http"
+	"net/http/pprof"
 	"time"
 )
 
@@ -18,23 +21,38 @@ func Time() {
 	}
 }
 
+// TimeAfter
+// debug cpu profiler NewTimer 占比很大
+// time.After 和 time.AfterFunc 均有定时器泄露风险，尤其在遍历和用户量大的的时候使用，会反复创建 timer 且不会释放，
+// 表现为服务器卡顿，在一定周期内CPU占用渐进式上升
 func TimeAfter() {
-	stop := make(chan struct{}, 1)
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/debug/pprof/pl", pprof.Profile)
+
 	go func() {
-		time.Sleep(60 * time.Second)
-		stop <- struct{}{}
-	}()
-
-	for {
-		select {
-		case <-time.After(time.Millisecond):
-
-		case <-stop:
-			fmt.Println("time over time after")
+		if err = http.Serve(ln, nil); err != nil {
 			return
 		}
+	}()
+
+	n := make(chan int, 10000)
+	i := 0
+	go func() {
+		for {
+			i++
+			n <- i
+		}
+	}()
+	for {
+		select {
+		case <-time.After(500 * time.Millisecond):
+		case <-n:
+			continue
+		}
 	}
-	// debug cpu profiler NewTimer 占比很大
-	// time.After 和 time.AfterFunc 均有定时器泄露风险，尤其在遍历和用户量大的的时候使用，会反复创建 timer 且不会释放，
-	// 表现为服务器卡顿，在一定周期内CPU占用渐进式上升
+
 }
