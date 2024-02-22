14fd9aa59fae6dd11785bd64fc069f781b495778
Victor <vitory.dong@gmail.com>
2024-02-22 16:44:40 +0800

    trace;

---
+++ gettrace/get_trace.go
@@ -0,0 +1,29 @@
+package gettrace
+
+import (
+	"os"
+	"runtime"
+	"runtime/trace"
+)
+
+func Trace() {
+
+	runtime.GOMAXPROCS(1)
+
+	file, err := os.OpenFile("./trace.out", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	trace.Start(file)
+
+	ch := make(chan string)
+	go func() {
+		ch <- "trace"
+	}()
+	<-ch
+
+	trace.Stop()
+
+}
