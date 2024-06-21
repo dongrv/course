704aefd6aa394c44fd224f50d02a5ddd4b58f11e
Victor <vitory.dong@gmail.com>
2024-06-21 17:03:59 +0800

    Add logger.Sync();

--- ginx/gin.go
+++ ginx/gin.go
@@ -12,7 +12,7 @@ import (
 // 获取日志管理器
 func initZapLogger() (*zap.Logger, error) {
 	config := zap.NewDevelopmentConfig()                         // NewProductionConfig
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // 自定义时间编码器
+	config.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder // 自定义时间编码器
 	config.Level.SetLevel(zapcore.InfoLevel)
 
 	// 初始化 Zap logger
@@ -32,20 +32,23 @@ func Run() {
 		println("Init logger err:", err.Error())
 		return
 	}
+	defer logger.Sync() // 程序退出时确保日志刷新到存储介质
+
 	r := gin.Default()
 	gin.SetMode(gin.DebugMode)
+
 	r.Use(authMiddleware, returnZapLoggerMiddleware(logger))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	fmt.Print("[GIN]Server started.\n")
+
 	if err := r.Run(":8086"); err != nil {
 		fmt.Printf("[GIN]Server run error found:%s", err.Error())
 		return
 	}
-	fmt.Print("[GIN]Server exited successfully.")
 }
 
 func authMiddleware(c *gin.Context) {
