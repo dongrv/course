316572dee01c10bf0dff73d950a412199ce62ea0
dongrvv <vitory.dong@gmail.com>
2024-08-15 12:11:30 +0800

    wasm;

--- wasm/main.go
+++ wasm/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	listen := ":8080"
-	dir := "./wasm/html" // 相对curse根目录
+	dir := "./wasm/assets" // 相对curse根目录
 	log.Printf("listening on %q...", ":8080")
 	err := http.ListenAndServe(listen, http.FileServer(http.Dir(dir)))
 	log.Fatalln(err)
